fbee24d228152c6222fd0513bcbe49b7c99558cb
stgreen <stgreen@ncsu.edu>
2017-09-16 22:19:23 +0800

    Finish multiblock detection

--- me/TehGoldyLockz/OlympicHeroes/listeners/MultiBlockListener.java
+++ me/TehGoldyLockz/OlympicHeroes/listeners/MultiBlockListener.java
@@ -4,9 +4,11 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.material.Stairs;
 
 import me.TehGoldyLockz.OlympicHeroes.OlympicHeroes;
 
@@ -22,12 +24,12 @@ public class MultiBlockListener implements Listener{
 		Material type = block.getType();
 		
 		if(type == Material.IRON_BLOCK) {
-			if(isShrine(block, Material.STONE, Material.SPRUCE_WOOD_STAIRS)) {
-				e.getPlayer().sendMessage("You just made a shrine to artemis!");
+			if(isShrine(block, Material.STONE, (byte) 4, Material.SPRUCE_WOOD_STAIRS)) {
+				e.getPlayer().sendMessage("You just made a shrine to Artemis!");
 			}
 		}else if(type == Material.GOLD_BLOCK) {
-			if(isShrine(block, Material.QUARTZ_BLOCK, Material.SANDSTONE_STAIRS)) {
-				e.getPlayer().sendMessage("You just made a shrine to apollo!");
+			if(isShrine(block, Material.QUARTZ_BLOCK, (byte) 0, Material.SANDSTONE_STAIRS)) {
+				e.getPlayer().sendMessage("You just made a shrine to Apollo!");
 			}
 		}else if(type == Material.DIAMOND_BLOCK) {
 			// hera
@@ -42,7 +44,8 @@ public class MultiBlockListener implements Listener{
 		}
 	}
 	
-	public boolean isShrine(Block block, Material exterior, Material stair) {
+	@SuppressWarnings("deprecation")
+	public boolean isShrine(Block block, Material exterior, byte exteriorData, Material stair) {
 		World w = block.getWorld();
 		Location l = block.getLocation();
 		if(w.getBlockAt(l.clone().add(0, 1, 0)).getType() == exterior && w.getBlockAt(l.clone().add(0, -1, 0)).getType() == exterior) {
@@ -52,13 +55,26 @@ public class MultiBlockListener implements Listener{
 			if(w.getBlockAt(l.clone().add(1, 0, 0)).getType() == exterior && 
 			   w.getBlockAt(l.clone().add(-1, 0, 0)).getType() == exterior &&
 			   w.getBlockAt(l.clone().add(1, -1, 0)).getType() == exterior &&
-			   w.getBlockAt(l.clone().add(-1, -1, 0)).getType() == exterior) {
+			   w.getBlockAt(l.clone().add(-1, -1, 0)).getType() == exterior &&
+			   w.getBlockAt(l.clone().add(1, 0, 0)).getData() == exteriorData && 
+			   w.getBlockAt(l.clone().add(-1, 0, 0)).getData() == exteriorData &&
+			   w.getBlockAt(l.clone().add(1, -1, 0)).getData() == exteriorData &&
+			   w.getBlockAt(l.clone().add(-1, -1, 0)).getData() == exteriorData) {
 				for(int z = -1; z <= 1; z+=2) {
-					if(w.getBlockAt(l.clone().add(0, -2, z).clone()).getType() == stair &&
-					   w.getBlockAt(l.clone().add(1, -1, z).clone()).getType() == stair &&
-					   w.getBlockAt(l.clone().add(-1, -1, z).clone()).getType() == stair) {
-						isShrine = true;
-					}
+					if(w.getBlockAt(l.clone().add(0, -2, z)).getType() == stair &&
+					   w.getBlockAt(l.clone().add(1, -1, z)).getType() == stair &&
+					   w.getBlockAt(l.clone().add(-1, -1, z)).getType() == stair) {
+						if( ( (Stairs) w.getBlockAt(l.clone().add(0, -2, z)).getState().getData() ).isInverted() &&
+							( (Stairs) w.getBlockAt(l.clone().add(1, -1, z)).getState().getData() ).getAscendingDirection() == BlockFace.EAST &&
+							( (Stairs) w.getBlockAt(l.clone().add(-1, -1, z)).getState().getData() ).getAscendingDirection() == BlockFace.WEST){
+							// z = -1: north ; z = 1: south
+							if(z == -1 &&  ((Stairs) w.getBlockAt(l.clone().add(0, -2, z)).getState().getData() ).getAscendingDirection() == BlockFace.SOUTH) {
+								isShrine = true;
+							}else if(z == 1 &&  ((Stairs) w.getBlockAt(l.clone().add(0, -2, z)).getState().getData() ).getAscendingDirection() == BlockFace.NORTH){
+								isShrine = true;
+							}
+						}
+					}	
 				}
 			}
 			
@@ -66,12 +82,25 @@ public class MultiBlockListener implements Listener{
 			if(w.getBlockAt(l.clone().add(0, 0, 1)).getType() == exterior && 
 			   w.getBlockAt(l.clone().add(0, 0, -1)).getType() == exterior &&
 			   w.getBlockAt(l.clone().add(0, -1, 1)).getType() == exterior &&
-			   w.getBlockAt(l.clone().add(0, -1, -1)).getType() == exterior) {
+			   w.getBlockAt(l.clone().add(0, -1, -1)).getType() == exterior &&
+			   w.getBlockAt(l.clone().add(0, 0, 1)).getData() == exteriorData && 
+			   w.getBlockAt(l.clone().add(0, 0, -1)).getData() == exteriorData &&
+			   w.getBlockAt(l.clone().add(0, -1, 1)).getData() == exteriorData &&
+			   w.getBlockAt(l.clone().add(0, -1, -1)).getData() == exteriorData) {
 				for(int x = -1; x <= 1; x+=2) {
 					if(w.getBlockAt(l.clone().add(x, -2, 0)).getType() == stair &&
 					   w.getBlockAt(l.clone().add(x, -1, 1)).getType() == stair &&
 					   w.getBlockAt(l.clone().add(x, -1, -1)).getType() == stair) {
-						isShrine = true;
+						if( ( (Stairs) w.getBlockAt(l.clone().add(x, -2, 0)).getState().getData() ).isInverted() &&
+							( (Stairs) w.getBlockAt(l.clone().add(x, -1, -1)).getState().getData() ).getAscendingDirection() == BlockFace.NORTH &&
+							( (Stairs) w.getBlockAt(l.clone().add(x, -1, 1)).getState().getData() ).getAscendingDirection() == BlockFace.SOUTH) {
+							// z = -1: west ; z = 1: east
+							if(x == -1 &&  ((Stairs) w.getBlockAt(l.clone().add(x, -2, 0)).getState().getData() ).getAscendingDirection() == BlockFace.EAST) {
+								isShrine = true;
+							}else if(x == 1 &&  ((Stairs) w.getBlockAt(l.clone().add(x, -2, 0)).getState().getData() ).getAscendingDirection() == BlockFace.WEST){
+								isShrine = true;
+							}
+						}
 					}
 				}
 				
